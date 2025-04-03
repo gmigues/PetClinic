40916d6f72ddf3b220e480bc413a1a078ddbe8bb
Gonzalo <gonzalo@proyectocf.com>
2025-04-03 16:28:29 -0300

    removing outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,1 @@
-# output "private_subnets_cidrs" {
-#   description = "CIDR blocks de las subnets privadas"
-#   value       = module.vpc.private_subnets_cidrs
-# }
 
-# output "public_subnets_cidrs" {
-#   description = "CIDR blocks de las subnets públicas"
-#   value       = module.vpc.public_subnets_cidrs
-# }
-
-# output "cluster_name" {
-#   description = "Nombre del cluster EKS"
-#   value       = module.eks.cluster_name
-# }
-
-# output "kubeconfig_command" {
-#   description = "Ejecuta este comando para actualizar tu kubeconfig"
-#   value       = module.eks.kubeconfig_command
-# }
