672fe711cbc067053c2dfab79e966dece65fec4c
Gonzalo <gonzalo@proyectocf.com>
2025-04-13 12:49:24 -0300

    1st try with terraform cicd

--- terraform/env/prod/vpc/main.tf
+++ terraform/env/prod/vpc/main.tf
@@ -14,35 +14,5 @@ module "vpc_module" {
 
 
 
-# # Grupo de seguridad para los nodos de EKS
-# resource "aws_security_group" "eks_nodes_sg" {
-#   name        = "eks-nodes-sg"
-#   description = "Security group for EKS nodes"
-#   vpc_id      = module.vpc_module.vpc_id # Asume que tu módulo VPC tiene una salida para la VPC ID
 
-#   tags = {
-#     Name = "EKS Nodes Security Group"
-#   }
-# }
-
-# # Reglas de seguridad para el grupo de seguridad de EKS
-# resource "aws_security_group_rule" "eks_nodes_ingress_self" {
-#   description       = "Allow node to node communication"
-#   type              = "ingress"
-#   from_port         = 0
-#   to_port           = 0
-#   protocol          = "-1"
-#   security_group_id = aws_security_group.eks_nodes_sg.id
-#   self              = true
-# }
-
-# resource "aws_security_group_rule" "eks_nodes_egress" {
-#   description       = "Allow all outbound traffic"
-#   type              = "egress"
-#   from_port         = 0
-#   to_port           = 0
-#   protocol          = "-1"
-#   security_group_id = aws_security_group.eks_nodes_sg.id
-#   cidr_blocks       = ["0.0.0.0/0"]
-# }
 
